93a54d78d99e6c33f5ea5a9e295b4191443e5ad3
github username <github email address>
2017-12-01 09:59:08 -0500

    Fixed json payload from individual users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :boardgames, through: :user_boardgames
 
   has_secure_password
+
+  def package_json
+    self.user_boardgames.map do|user_boardgame|
+      raw_json = {info: user_boardgame, game: user_boardgame.boardgame}
+    end
+  end
 end
