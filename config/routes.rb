cbc50a27cf8d45c7a0596173b95dcbf6584f852c
github username <github email address>
2017-12-02 19:10:07 -0500

    jwt token correctly returns

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
       get '/users/:id/createboardgame' => 'users#createBoardgame'
       post '/users/:id/createboardgame' => 'users#createBoardgame'
-      post '/auth', to: 'sessions#create'
-      get '/current_user', to: 'sessions#show'
+      post '/auth' => 'sessions#create'
+      get '/current_user' => 'sessions#show'
     end
   end
 end
