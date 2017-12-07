a36417634963b102808d848361cae2ce8b7c5115
Ashlee Crusco <ashleecrusco@gmail.com>
2017-12-07 09:43:38 -0500

    added username validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
   has_secure_password
 
+  validates :username, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def package_json
     self.user_boardgames.map do|user_boardgame|
       raw_json = {info: user_boardgame, game: user_boardgame.boardgame}
